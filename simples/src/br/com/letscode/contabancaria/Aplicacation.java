58d21d3c6f3f794a19d1e641aac4d9b6b8ebc575
Rapha Mendonca <raphamendonca@protonmail.com>
2022-01-19 18:56:50 -0300

    Projeto exemplo

---
+++ simples/src/br/com/letscode/contabancaria/Aplicacation.java
@@ -0,0 +1,4 @@
+package br.com.letscode.contabancaria;
+
+public class Aplicacation {
+}
---
+++ simples/src/br/com/letscode/contabancaria/conta/Titular.java
@@ -0,0 +1,37 @@
+package br.com.letscode.contabancaria.conta;
+
+public class Titular {
+    private String nome;
+    private String email;
+    private String endereco;
+
+    public Titular(String nome, String email, String endereco) {
+        this.nome = nome;
+        this.email = email;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
